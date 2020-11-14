e1a91bcad5bff86a39c9c03bd01618e527d37ecc
Hongwei <hongweiyeung@yahoo.com>
2020-11-13 21:39:58 -0700

    Added graph axies

--- js/Graph.js
+++ js/Graph.js
@@ -1,38 +1,70 @@
 class Graph {
 
-    constructor(data)
-    {
+    constructor(data) {
         this.data = data;
 
-        console.log(data); 
+        console.log(data);
 
 
+        let max = d3.max(data, function (d) { return +d.suicides_no });
+
+        //sucide 
         this.scaleY = d3.scaleLinear()
-        .domain([-100, 100])
-        .range([0, 30]);
+            .domain([max, 0]) //max = 22338
+            .range([0, 400]); //will need to change later 
 
+        //time 
         this.scaleX = d3.scaleLinear()
-        .domain([-100, 100])
-        .range([0, 30]);
+            .domain([1985, 2016])
+            .range([0, 500]); //will need to change later 
+
+        //sucide over time 
+        //gdp over time 
+        //filter over 
+        d3.select('#graph').append("svg").attr("id", "graph_svg").attr("height", 500).attr("width", 600);
+
+        this.drawLegend();
+        this.drawGraph(data);
     }
-    
-    drawLegend()
-    {
 
+    //need? if were changing the y axis a lot? 
+    drawLegend() {
 
+        let svg = d3.select("#graph_svg");
 
+        //x-axis
+        svg.append("g").attr("transform", "translate(50, 420)")
+            .call(d3.axisBottom(this.scaleX).ticks().tickFormat(d3.format("d")));
+
+        //y-axis
+        svg.append("g").attr("transform", "translate(50,20)")
+            .call(d3.axisLeft(this.scaleY).ticks(10));
     }
 
-    drawLines()
-    {
+    drawGraph(data) {
+
+        let svg = d3.select("#graph_svg");
+
+        let LineGenerator = d3
+            .line()
+            .x((d, i) => scaleX(i))
+            .y(d => scaleY(d.suicides_no));
+
+        d3.select("#aLineChart")
+            .data(data)
+            .transition().duration(2000)
+            .attr("d", aLineGenerator(data));
+    }
+
+    //? 
+    drawLines() {
 
 
 
     }
 
     //don't know if we need this or not, just blocking out some code
-    toggleStory()
-    {
+    toggleStory() {
 
     }
 }
