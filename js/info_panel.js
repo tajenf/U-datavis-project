dd1e880f4f4fde3a3235412a82f4c1cb9eb6e39a
BakaWisdom <42656515+BakaWisdom@users.noreply.github.com>
2020-11-18 22:54:40 -0700

    info fillout/suicidedataupdate
    added a parameter to infopanel
    
    reordered suicide data so year is last to improve data access efficiency while its still easy
    
    Added a lot more details to the info panel and reformatted how updates occur to make life easier later.
    
    added some data structures
    
    Implimented a lot of the data in the info section.

--- js/info_panel.js
+++ js/info_panel.js
@@ -1,17 +1,25 @@
 class InfoPanel {
 
-    constructor(sdata, ydata)
+    constructor(sdata, ydata, ykeys, cdata, cKeys, ageGroups)
     {
-        this.yeardata = ydata;
-        this.suicides = sdata;
+        this.yearData = ydata;
+        this.yearKeys = ykeys;
+        this.suicideData = sdata;
+        this.countryData = cdata;
+        this.countryKeys = cKeys;
+        this.ageGroups = ageGroups;
+
+        console.log(this.ageGroups);
 
         this.detailPanel = d3.select("#detail");
 
         this.initialPopulate();
 
+
+
         this.country = "";
         this.year = "1987";
-        this.yearspan = 0;
+        this.yearSpan = 0;
     }
 
     initialPopulate()
@@ -23,60 +31,147 @@ class InfoPanel {
         detailPanel.append("div").text("Country: ").classed("cat", true)
             .append("div").text("curCountry").attr('id', "country").classed("data", true);
 
-        detailPanel.append("div").text("Suicides: ").classed("cat", true)
-            .append("div").text("curSuicides").attr('id', "suicide").classed("data", true);
+        //forloop of country specific information
 
-            detailPanel.append("div").text("Population: ").classed("cat", true)
-                .append("div").text("curPop").attr('id', "pop").classed("data", true);
+        detailPanel.append("div").text("year: ").attr('id', "yearlabel").classed("cat", true)
+            .append("div").text("curCountry").attr('id', "year").classed("data", true);
+
+        detailPanel.append("div").text("Total Suicides: ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "totalSui").classed("data", true);
+
+        detailPanel.append("div").text("Population: ").classed("cat", true)
+            .append("div").text("curPop").attr('id', "totalPop").classed("data", true);
+
+        detailPanel.append("div").text("GDP: ").classed("cat", true)
+            .append("div").text("curGDP").attr('id', "gdp").classed("data", true);
+
+        let males = detailPanel.append('g').attr('id', "male");
+
+        males.append("div").text("Male Population: ").classed("cat", true)
+            .append("div").text("curPop").attr('id', "malePop").classed("data", true);
+            
+        males.append("div").text("Total Male Suicides: ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "maleSui").classed("data", true);
+            
+        males.append("div").text("Breakdown: ").classed("cat", true)
+
+        males.append("div").text("-Ages 5-14 : ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "a5-14_years").classed("data", true);
+
+        males.append("div").text("-Ages 15-24: ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "a15-24_years").classed("data", true);
+
+        males.append("div").text("-Ages 25-34: ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "a25-34_years").classed("data", true);
+
+        males.append("div").text("-Ages 35-54: ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "a35-54_years").classed("data", true);
+
+        males.append("div").text("-Ages 55-74: ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "a55-74_years").classed("data", true);
+
+        males.append("div").text("-Ages 75+ : ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "a75+_years").classed("data", true);
+
+        let females = detailPanel.append('g').attr('id', "female");
+
+        females.append("div").text("Female Population: ").classed("cat", true)
+            .append("div").text("curPop").attr('id', "femalePop").classed("data", true);
+            
+        females.append("div").text("Total Female Suicides: ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "femaleSui").classed("data", true);
+            
+        females.append("div").text("Breakdown: ").classed("cat", true)
+
+        females.append("div").text("-Ages 5-14 : ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "a5-14_years").classed("data", true);
+
+        females.append("div").text("-Ages 15-24: ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "a15-24_years").classed("data", true);
+
+        females.append("div").text("-Ages 25-34: ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "a25-34_years").classed("data", true);
+
+        females.append("div").text("-Ages 35-54: ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "a35-54_years").classed("data", true);
+
+        females.append("div").text("-Ages 55-74: ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "a55-74_years").classed("data", true);
+
+        females.append("div").text("-Ages 75+ : ").classed("cat", true)
+            .append("div").text("curSuicides").attr('id', "a75+_years").classed("data", true);
+
+        
     }
 
     UpdateCountry(countryID)
     {
         this.country = countryID;
 
-        this.UpdatePanel();
+        this.UpdateAllInfo();
     }
 
     UpdateYear(year, span)
     {
         this.year = year;
-        this.span = span;
+        this.yearSpan = span;
 
-        this.UpdatePanel();
+        
+        let detailPanel = d3.select("#detail");
+
+        this.UpdateYearInfo(detailPanel);
+        this.UpdateSuicides(detailPanel);
     }
 
-    UpdatePanel(){
+    UpdateAllInfo()
+    {
         let detailPanel = d3.select("#detail");
 
         detailPanel.select("#country").text(this.country);
 
-        if (this.yeardata[this.country]) {
-            detailPanel.select("#pop").text(this.yeardata[this.country][this.year]["population"]);
-            
-            let suicides = this.findSuicides(this.country);
-            detailPanel.select("#suicide").text(suicides);
+        this.UpdateSuicides(detailPanel);
+        
+        this.UpdateYearInfo(detailPanel);
+    }
+
+    UpdateYearInfo(panel)
+    {
+        let yearlabel = panel.select("#yearlabel")
+        let year = panel.select("#year");
+        if (this.yearSpan == 0) {
+            yearlabel.text("year: ");
+            year.text(this.year);
         } else {
-            detailPanel.select("#pop").text("N/A");
-            detailPanel.select("#suicide").text("N/A");
+            yearlabel.text("years: ");
+            year.text(`${this.year}-${this.year + this.yearSpan}`)
+        }
+
+        if (this.country == "") {
+            return;
+        }
+
+        if (this.yearData[this.country] && this.yearData[this.country][this.year])
+        {
+            Object.keys(this.yearData[this.country][this.year]).forEach(key => {
+                d3.select(`#${key}`).text(this.yearData[this.country][this.year][key]);
+            });
+        } else 
+        {
+            Object.keys(this.yearData[this.country][this.year]).forEach(key => {
+                d3.select(`#${key}`).text("N/A");
+            });
         }
     }
 
-    findSuicides(countryID)
+    UpdateSuicides(panel)
     {
-        if (this.sex === "both") {
-            return this.calcSuicides(this.suicides[countryID][this.year]["male"]) + this.calcSuicides(this.suicides[countryID][this.year]["female"]);
-        } else {
-            return this.calcSuicides(this.suicides[countryID][this.year][this.sex]);
-        }
-    }
-
-    calcSuicides(data)
-    {
-        //TODO only add specific ages
-        let output = 0;
-        Object.keys(data).forEach(key => {
-            output += data[key][0];
-        });
-        return output;
+        let male = panel.select("#male");
+        let female = panel.select("#female");
+        this.ageGroups.forEach( age =>
+            {
+                console.log(age);
+                male.select(`#${age}`).text(this.suicideData[this.country]["male"][age][this.year]["suicides"]);
+                female.select(`#${age}`).text(this.suicideData[this.country]["female"][age][this.year]["suicides"]);
+            });  
     }
 }
--- js/script.js
+++ js/script.js
@@ -6,8 +6,20 @@ Promise.all([data]).then(data =>
         let oData = [];
         let countryName = "";
         let startIndex = -1;
+
+        //data that we have multiple years for
         let yearData = {};
+
+        //suicide data organized as follows
+        // country -> sex -> age group -> year
         let suicideData = {};
+
+        //data specific to a country that won't change over time
+        let countryData = {};
+        //all possible subkeys for country data
+        let countryKeys = {};
+        //all possible subkeys for yearly data
+        let yearKeys = new Set();
 
         for (let i = 0; i < data[0].length; i++)
         {
@@ -23,6 +35,9 @@ Promise.all([data]).then(data =>
             }
         }
 
+        let years = new Set();
+        let ageGroups = new Set();
+
         data[0].forEach(element => {
             
             if (!yearData[element.country]) {
@@ -32,24 +47,69 @@ Promise.all([data]).then(data =>
             let population = element.population;
             let gdp = element["gdp_per_capita ($)"];
 
-            yearData[element.country][element.year] = {population, gdp};
+            yearData[element.country][element.year] = {gdp};
 
             if (!suicideData[element.country]) {
                 suicideData[element.country] = {};
             }
 
-            if (!suicideData[element.country][element.year]) {
-                suicideData[element.country][element.year] = {};
+            if (!suicideData[element.country][element.sex]) {
+                suicideData[element.country][element.sex] = {};
             }
 
-            if (!suicideData[element.country][element.year][element.sex]) {
-                suicideData[element.country][element.year][element.sex] = {};
+            if (!suicideData[element.country][element.sex]["a" + element.age.replace(" ", "_")]) {
+                suicideData[element.country][element.sex]["a" + element.age.replace(" ", "_")] = {};
             }
 
             let suicides = parseInt(element.suicides_no);
             let popsuicides = parseFloat(element["suicides/100k pop"]);
 
-            suicideData[element.country][element.year][element.sex][element.age] = [suicides, popsuicides];
+            suicideData[element.country][element.sex]["a" + element.age.replace(" ", "_")][element.year] = {suicides, popsuicides, population};
+
+            years.add(element.year);
+            ageGroups.add("a" + element.age.replace(" ", "_"));
+        });
+
+        yearKeys.add("gdp");
+        yearKeys.add("malePop");
+        yearKeys.add("femalePop");
+        yearKeys.add("totalPop");
+        yearKeys.add("maleSui");
+        yearKeys.add("femaleSui");
+        yearKeys.add("totalSui");
+
+        years.forEach( year => {
+
+            Object.keys(suicideData).forEach(country => {
+
+                if(yearData[country][year])
+                {
+                    let malePop = 0;
+                    let femalePop = 0;
+                    let maleSui = 0;
+                    let femaleSui = 0;
+
+                    ageGroups.forEach(age => {
+                        if(suicideData[country]["male"][age] && suicideData[country]["male"][age][year])
+                        {
+                            malePop += parseInt(suicideData[country]["male"][age][year]["population"]);
+                            femalePop += parseInt(suicideData[country]["female"][age][year]["population"]);
+                            maleSui += parseInt(suicideData[country]["male"][age][year]["suicides"]);
+                            femaleSui += parseInt(suicideData[country]["female"][age][year]["suicides"]);
+                        }
+                    });
+
+                    yearData[country][year]["malePop"] = malePop;
+                    yearData[country][year]["femalePop"] = femalePop;
+                    yearData[country][year]["totalPop"] = malePop + femalePop;
+
+                    yearData[country][year]["maleSui"] = maleSui;
+                    yearData[country][year]["femaleSui"] = femaleSui;
+                    yearData[country][year]["totalSui"] = maleSui + femaleSui;
+                }
+
+                
+            });
         });
 
         function UpdateCountry(country) {
@@ -74,7 +134,7 @@ Promise.all([data]).then(data =>
 
         let world = new World(oData, UpdateCountry);
         let graph = new Graph(data[0]);
-        let info = new InfoPanel(suicideData, yearData);
+        let info = new InfoPanel(suicideData, yearData, yearKeys, countryData, countryKeys, ageGroups);
 
         d3.json('./data/countries.geojson').then(map => { world.drawWorld(map)});
 
