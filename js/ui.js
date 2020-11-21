6e1ca6af617b5621b218c98d5a5594c4d349efbb
Hongwei <hongweiyeung@yahoo.com>
2020-11-21 16:58:44 -0700

    Have proper year values for year slider

--- js/ui.js
+++ js/ui.js
@@ -61,37 +61,39 @@ class UI {
 
 
         let range1 = d3.select("#range1");
-
-        // range1.attr("oninput", function(){
-        //     console.log(this.max);
-        //     console.log(this.value);
-        //     console.log(this.parentNode);
-        //     console.log(this.parentNode.childNodes); 
-        //     console.log(this.parentNode.childNodes[4]); 
-        //     let children = this.parentNode.childNodes[1].childNodes; 
-        //     console.log(children); 
-        //     console.log(children[11]);
-        //     return ("this.value=Math.min(this.value,this.parentNode.childNodes[5].value-1);"+
-        //     "var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);"+
-        //     "var children = this.parentNode.childNodes[1].childNodes;"+
-        //     "children[1].style.width=value+'%';"+
-        //     "children[5].style.left=value+'%';"+
-        //     "children[7].style.left=value+'%';"+
-        //     "children[11].style.left=value+'%';"+
-        //     "children[11].childNodes[1].innerHTML=this.value;")
-        // });
+        let range2 = d3.select("#range2");
 
 
+        range1.attr("oninput", function(){
+            // console.log(this.max);
+            // console.log(this.value);
+            // console.log(this.parentNode);
+            // console.log(this.parentNode.childNodes); 
+            // console.log(this.parentNode.childNodes[4]); 
+            // let children = this.parentNode.childNodes[1].childNodes; 
+            // console.log(children); 
+            // console.log(children[11]);
+            return ("this.value=Math.min(this.value,this.parentNode.childNodes[5].value-1);"+
+            "var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);"+
+            "var children = this.parentNode.childNodes[1].childNodes;"+
+            "children[1].style.width=value+'%';"+
+            "children[5].style.left=value+'%';"+
+            "children[7].style.left=value+'%';"+
+            "children[11].style.left=value+'%';"+
+            "children[11].childNodes[1].innerHTML= this.value;")
+        });
 
 
-        // this.value=Math.min(this.value,this.parentNode.childNodes[5].value-1);
-        // var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
-        // var children = this.parentNode.childNodes[1].childNodes;
-        // children[1].style.width=value+'%';
-        // children[5].style.left=value+'%';
-        // children[7].style.left=value+'%';
-        // children[11].style.left=value+'%';
-        // children[11].childNodes[1].innerHTML=this.value;"
+        range2.attr("oninput", function(){
+            return ("this.value=Math.max(this.value,this.parentNode.childNodes[3].value-(-1));"+
+            "var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);"+
+             "var children = this.parentNode.childNodes[1].childNodes;"+
+             "children[3].style.width=(100-value)+'%';"+
+             "children[5].style.right=(100-value)+'%'; "+
+             "children[9].style.left=value+'%'; "+
+             "children[13].style.left=value+'%'; "+
+             "children[13].childNodes[1].innerHTML=this.value;")
+        });
     }
 
 
